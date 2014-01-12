60029acbf117a186c99320393a6dc0379b4c08ff
Mr. Russian <mr_russian@rocketmail.com>
2014-01-12 14:47:49 -0800

    Initial Commit

---
+++ Equalizer/EQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EQAppDelegate.h
+//  Equalizer
+//
+//  Created by Max Nedorezov on 1/12/14.
+//  Copyright (c) 2014 Max Nedorezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Equalizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Equalizer
+//
+//  Created by Max Nedorezov on 1/12/14.
+//  Copyright (c) 2014 Max Nedorezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EQAppDelegate class]));
+    }
+}
